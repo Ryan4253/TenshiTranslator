b7d2d6cbf3de8264e5c0891283a9472a99effbf2
Ryan4253 <ryan.liao0305@gmail.com>
2023-11-27 21:07:17 -0500

    Write TextProcess unit tests

---
+++ test/Util/TestGlossary.py
@@ -0,0 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import unittest
+from TenshiTranslator.Util.Glossary import Glossary
--- test/Util/TestTextProcessor.py
+++ test/Util/TestTextProcessor.py
@@ -2,16 +2,14 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import context
 import unittest
 from TenshiTranslator.Util.TextProcessor import *
-import os
 
 class TextProcessorTest(unittest.TestCase):
     def testRetrieveLines(self):
         lines = retrieveLines("test/assets/SampleInput.txt")
         self.assertEqual(lines, [
-            "紙をペン先が撫で、カッカッとやや硬質な音を立てながら真っ白なページを文字で埋めていく。\n", 
+            "しゅくだいしているから邪魔じゃましないでくれ。\n", 
             "\n", 
             "◇◇◇◇"])
 
@@ -21,26 +19,30 @@ class TextProcessorTest(unittest.TestCase):
 
     def testIsEmptyLine(self):
         self.assertTrue(isEmptyLine("      "))
-        self.assertFalse(isEmptyLine("wad"))
+        self.assertFalse(isEmptyLine("   wad   "))
 
     def testRemoveIndent(self):
         self.assertEqual(removeIndent("　hello world"), "hello world")
         self.assertEqual(removeIndent("hello world"), "hello world")
 
     def testIsTimeoutMessage(self):
-        self.assertTrue(isTimeoutMessage(
-            "Too many requests from this IP, please try again after 5 minutes. BTW, If you think the translation \
-            quality is SUGOI, feel free to join our discord server at: https://discord.gg/XFbWSjMHJh"))
+        self.assertTrue(isTimeoutMessage("Too many requests from this IP, please try again after 5 minutes. BTW, If you think the translation \
+            quality is SUGOI, feel free to join our discord server at: https://discord.gg/XFbWSjMHJh")
+        )
         self.assertFalse(isTimeoutMessage("literally anything else"))
 
     def testSplitToSentence(self):
-        self.assertEqual(splitToSentence("夕食後、後片付けも終わった後は、一人で筧くのだが、常に二人でくっつき合っている訳ではない。\
-                                         クラスメイト達はおろか樹まで勘違いしていたので笑っていいのか思いのが。\
-                                         周達は四六時中いちゃついていると思われているらしい。", 100))
+        data = "しゅくだいしているから邪魔じゃましないでくれ。しゅくだいしているから邪魔じゃましないでく。しゅくだいしているから邪魔じゃましないで。しゅくだいしているから邪魔じゃましない。しゅくだいしているから邪魔じゃましな。"
+        self.assertEqual(splitToSentence(data, 100), 
+                        ["しゅくだいしているから邪魔じゃましないでくれ。",
+                         "しゅくだいしているから邪魔じゃましないでく。",
+                         "しゅくだいしているから邪魔じゃましないで。",
+                         "しゅくだいしているから邪魔じゃましない。",
+                         "しゅくだいしているから邪魔じゃましな。"])
         
+        self.assertEqual(splitToSentence(data, 500), [data])
         
-
     def testNoJapaneseCharacters(self):
         self.assertTrue(noJapaneseCharacters("◇◇◇◇"))
         self.assertTrue(noJapaneseCharacters("test"))
-        self.assertFalse(noJapaneseCharacters("紙をペン先が撫で、"))
+        self.assertFalse(noJapaneseCharacters("しゅくだいしているから邪魔じゃましないでくれ。"))
