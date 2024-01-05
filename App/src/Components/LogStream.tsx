eb0bb0f8a616d694a4ce85704e39c04a2605ca08
Ryan4253 <ryan.liao0305@gmail.com>
2024-01-05 07:23:09 -0500

    #3 Apply proper spacing to LogStream

--- App/src/Components/LogStream.tsx
+++ App/src/Components/LogStream.tsx
@@ -1,34 +1,35 @@
 import { useState, useEffect, useRef, Fragment } from 'react';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import { IpcRendererEvent } from 'electron';
 const { ipcRenderer } = window.require('electron');
 
 const LogStream = () =>{
-  const [textStream, setTextStream] = useState<string[]>([]);
-  const paperRef = useRef<HTMLDivElement>(null);
+    const [textStream, setTextStream] = useState<string[]>([]);
+    const paperRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const handleTextStream = (event: any, text: string) => {
-        setTextStream((prevTextStream) => [...prevTextStream, text]);
+    useEffect(() => {
+        const handleTextStream = (event: IpcRendererEvent, text: string) => {
+            setTextStream((prevTextStream) => [...prevTextStream, text]);
 
-        setTimeout(() => {
-            if (paperRef.current) {
-                paperRef.current.scrollTop = paperRef.current.scrollHeight;
-            }
-        }, 5);
-    };
+            setTimeout(() => {
+                if (paperRef.current) {
+                    paperRef.current.scrollTop = paperRef.current.scrollHeight;
+                }
+            }, 5);
+        };
 
-    ipcRenderer.on('translationProcess', handleTextStream);
+        ipcRenderer.on('translationProcess', handleTextStream);
 
-    return () => {
-      ipcRenderer.removeListener('translationProcess', handleTextStream);
-    };
-  }, []);
+        return () => {
+        ipcRenderer.removeListener('translationProcess', handleTextStream);
+        };
+    }, []);
 
     return (
         <Paper ref={paperRef} elevation={3} sx={{width:"100vh", height:"30vh", backgroundColor:"#403e41", overflowY:"auto", p: 1, m:1}}>
             <Typography variant="h6" color="white" textAlign="left">Logs</Typography>
-            {textStream.map((text, index) => (
+            {textStream.map((text: string, index: number) => (
                 (text === '') ? index === textStream.length - 1 ? 
                 <div></div>: 
                 <Typography textAlign="left" dangerouslySetInnerHTML={{ __html: '&nbsp;' }} />:
