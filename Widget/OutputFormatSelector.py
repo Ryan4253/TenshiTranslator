ebd7beef00de5b8bb8e0616dec0e1787e9da4621
Ryan4253 <ryan.liao0305@gmail.com>
2024-05-23 11:44:50 -0400

    Reformat output selector

--- Widget/OutputFormatSelector.py
+++ Widget/OutputFormatSelector.py
@@ -5,32 +5,29 @@ class OutputFormatSelector(QWidget):
         super().__init__()
 
         self.lineByLine = QPushButton("Line By Line")
-        self.lineByLine.setCheckable(True)
-        self.lineByLine.setChecked(True)
-        self.lineByLine.clicked.connect(self.toggleLineByLine)
-
         self.englishOnly = QPushButton("English Only")
+
+        self.lineByLine.setCheckable(True)
         self.englishOnly.setCheckable(True)
-        self.englishOnly.clicked.connect(self.toggleEnglishOnly)
+
+        self.lineByLine.clicked.connect(lambda: self.toggle(0))
+        self.englishOnly.clicked.connect(lambda: self.toggle(1))
     
         layout = QHBoxLayout(self)
         layout.addWidget(self.lineByLine)
         layout.addWidget(self.englishOnly)
 
-    def toggleLineByLine(self):
-        sender = self.sender()
-        self.englishOnly.setChecked(False)
-        self.lineByLine.setChecked(True)
-    
-    def toggleEnglishOnly(self):
-        sender = self.sender()
-        self.englishOnly.setChecked(True)
-        self.lineByLine.setChecked(False)
+        self.toggle(0)
+
+    def toggle(self, index):
+        self.lineByLine.setChecked(index == 0)
+        self.englishOnly.setChecked(index == 1)
     
     def getOutputFormat(self):
         if self.lineByLine.isChecked():
             return "LineByLine"
-        elif self.englishOnly.isChecked():
+        
+        if self.englishOnly.isChecked():
             return "EnglishOnly"
-        else:
-            return None
+        
+        return None
