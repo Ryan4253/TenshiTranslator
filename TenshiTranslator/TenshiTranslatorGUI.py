a19be8c6db9ba3fb7281a9728354ffec2f5f7033
Ryan4253 <ryan.liao0305@gmail.com>
2024-05-25 11:37:04 -0400

    Allow freeze operation for executables

--- TenshiTranslator/TenshiTranslatorGUI.py
+++ TenshiTranslator/TenshiTranslatorGUI.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import multiprocessing
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from TenshiTranslator.UI.MainApplication import MainApplication
@@ -7,6 +8,7 @@ from TenshiTranslator.UI.MainApplication import MainApplication
 from PyQt6.QtWidgets import QApplication
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     app = QApplication([])
     widget = MainApplication()
     widget.show()
