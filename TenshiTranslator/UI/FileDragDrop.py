b0bb0f09fb526aa2739b6caf96f151b7c48ee8d8
Ryan4253 <ryan.liao0305@gmail.com>
2024-05-25 11:37:24 -0400

    Add watermark to indicate file area, allow click to add files

--- TenshiTranslator/UI/FileDragDrop.py
+++ TenshiTranslator/UI/FileDragDrop.py
@@ -3,8 +3,8 @@ from TenshiTranslator.UI.FileFrame import FileFrame
 import os
 
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QDragEnterEvent, QDropEvent
-from PyQt6.QtWidgets import QScrollArea, QWidget, QVBoxLayout
+from PyQt6.QtGui import QDragEnterEvent, QDropEvent, QMouseEvent
+from PyQt6.QtWidgets import QScrollArea, QWidget, QVBoxLayout, QFileDialog, QLabel
 
 class FileDropWidget(QScrollArea):
     def __init__(self):
@@ -13,13 +13,16 @@ class FileDropWidget(QScrollArea):
 
         self.container = QWidget()
 
-        self.containerLayout = QVBoxLayout(self.container)
-        self.containerLayout.setSpacing(10) 
-        self.containerLayout.setAlignment(Qt.AlignmentFlag.AlignTop)
-
         self.setAcceptDrops(True)
         self.setWidgetResizable(True)
         self.setWidget(self.container)
+
+        self.watermarkLabel = QLabel("Choose a file or drag it here")
+        
+        self.containerLayout = QVBoxLayout(self.container)
+        self.containerLayout.setSpacing(10) 
+        self.containerLayout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.containerLayout.addWidget(self.watermarkLabel)
 
     def dragEnterEvent(self, event: QDragEnterEvent):
         if event.mimeData().hasUrls():
@@ -33,6 +36,12 @@ class FileDropWidget(QScrollArea):
                 fileName = url.toLocalFile()
                 self.addFile(fileName)
 
+    def mousePressEvent(self, event: QMouseEvent):
+        if event.button() == Qt.MouseButton.LeftButton:
+            files, _ = QFileDialog.getOpenFileNames(self, "Select Files to Translate", "", "Text Files (*.txt)")
+            for file in files:
+                self.addFile(file)
+
     def addFile(self, fileName: str):
         if fileName in self.files:
             return
@@ -42,15 +51,23 @@ class FileDropWidget(QScrollArea):
         
         if not fileName.endswith(".txt"):
             return
-
+        
+        self.watermarkLabel.hide()
+        self.containerLayout.setAlignment(Qt.AlignmentFlag.AlignTop)
         fileFrame = FileFrame(fileName)
         self.containerLayout.addWidget(fileFrame)
         self.files.append(fileName)
-        
+
     def getFiles(self):
         return self.files
     
     def clearFiles(self):
         self.files.clear()
         for i in reversed(range(self.containerLayout.count())):
+            if i == 0:
+                continue
             self.containerLayout.itemAt(i).widget().setParent(None)
+
+        self.containerLayout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.watermarkLabel.setVisible(True)
+
